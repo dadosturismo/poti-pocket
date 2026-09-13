954c668a9adc985a19713ec62987c3979fc56107
dadosturismo <hakuhimanashi@gmail.com>
2026-09-13 00:11:07 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 /** Único ponto de configuração do front-end. */
 export const APP_CONFIG = Object.freeze({
-  WEB_APP_URL: 'https://script.google.com/macros/u/1/s/AKfycbxngKCRuvw6z63CuNcQ9TRTKo4QZNdz54P0OpNXISLJuCDHMqzru3AiIynQMbi80g6QZQ/exec',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxngKCRuvw6z63CuNcQ9TRTKo4QZNdz54P0OpNXISLJuCDHMqzru3AiIynQMbi80g6QZQ/exec',
   API_TIMEOUT_MS: 30000,
   DATABASE_NAME: 'turismo-curitiba-pocket-v1',
   DATA_KEY: 'dashboard-data'
